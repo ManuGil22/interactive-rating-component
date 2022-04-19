ba0b6bca907a80a640476a3d63207b4d528663c6
Manuel Gil <manuelgilmoltrasio@gmail.com>
2022-04-19 16:27:32 -0300

    Added thank-you card and js logic to enable-dissable cards

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 const rateBtns = document.querySelectorAll('.rate-btn');
+const submitBtn = document.querySelector('.submit-btn');
+const ratingCard = document.querySelector('.rating');
+const thankYouCard = document.querySelector('.thank-you');
+const endRating = document.querySelector('.end-rating');
 
 rateBtns.forEach((btn) => {
 	btn.addEventListener('click', (e) => {
@@ -7,8 +11,21 @@ rateBtns.forEach((btn) => {
 			button.classList.remove('rate-btn--checked');
 			button.ariaChecked = 'false';
 		});
-		// and adds checked class to clicked button
+		// adds checked class to clicked button
 		e.target.classList.add('rate-btn--checked');
 		e.target.ariaChecked = 'true';
 	});
+});
+
+submitBtn.addEventListener('click', (e) => {
+    // prevents reloading the page on submit
+	e.preventDefault();
+	// checks if any rating is selected
+    const checkedBtn = document.querySelector('.rate-btn--checked');
+    if (checkedBtn) {
+        const selectedRating = checkedBtn.value;
+        ratingCard.classList.toggle('card--hidden');
+        thankYouCard.classList.toggle('card--hidden');
+        endRating.innerText = selectedRating;
+    }
 });
